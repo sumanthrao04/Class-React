751734ccfddb90a83a4aa4642622741c78c35c91
sumanthrao04 <sumanthrao75@gmail.com>
2025-11-25 16:42:27 +0530

    first commit

---
+++ class-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+       <h1>Hello World</h1>
+        <p>Welcome to React with Vite!</p>
+        <p>First App</p>
+    </>
+  )
+}
+
+export default App
